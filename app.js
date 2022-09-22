017c02a21d6632d3d37bb670f7bca9b451622c85
Moisés Breton <moisesbreton11@gmail.com>
2022-09-21 22:20:55 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const ask = () => {
+    //alert('Holaaaaa!');
+    //Leer pregunta
+    const questionInput = document.getElementById("question")
+    const answerInput = document.getElementById('answer');
+    if(questionInput.value !==""){
+    //Recibir pregunta al preguntar a la bola magica
+    let answer = magicBall();
+    //imprimir resultado
+    answerInput.value = answer;
+} else {
+    answerInput.value = "Pregunta algo!!";
+}
+}
+
+//Funcion bola magica
+const magicBall = () => {
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber){
+  case 0:
+    eightBall ="It is certain";
+    break;
+  case 1:
+    eightBall ="It is decidedly so";
+    break;
+  case 2:
+    eightBall ="Reply hazy try again";
+    break;
+  case 3:
+    eightBall ="Cannot predict now";
+    break;
+  case 4:
+    eightBall ="Do not count on it";
+    break;
+  case 5:
+    eightBall ="My sources say no";
+    break;
+  case 6:
+    eightBall ="Outlook not so good";
+    break;
+  case 7:
+    eightBall ="Sings point to yes";
+    break;
+}
+return eightBall
+
+};
